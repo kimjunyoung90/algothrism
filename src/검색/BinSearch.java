1e098066d09848ad075e2db2e0370b33addc3086
김준영 <wnsdud1900427@gmail.com>
2023-05-28 18:47:48 +0900

    이진검색

---
+++ src/검색/BinSearch.java
@@ -0,0 +1,58 @@
+package 검색;
+
+import java.util.Scanner;
+
+/**
+ * 이진검색
+ * @author wnsdu
+ *
+ */
+public class BinSearch {
+	static int binSearch(int[] a, int n, int key) {
+		int pl = 0; //검색 시작 idx
+		int pr = n - 1; // 검색 마지막 idx
+		
+		do {
+			int pc = (pl + pr) / 2;
+			if(a[pc] == key) {
+				return pc;
+			}else if (a[pc] < key) {
+				pl = pc + 1;
+			}else {
+				pr = pc - 1;
+			}
+		}while (pl <= pr);
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("요소수 : ");
+		int num = stdIn.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력하세요.");
+		
+		System.out.print("x[0] : ");
+		x[0] = stdIn.nextInt();
+		for(int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "] : ");
+				x[i] = stdIn.nextInt();
+			}while(x[i] < x[i -1]); // 바로 앞의 요소보다 작으면 다시 입력
+		}
+		
+		System.out.print("검색할 값 : ");
+		int ky = stdIn.nextInt();
+		
+		int idx = binSearch(x, num, ky);
+		
+		if(idx == -1) {
+			System.out.println("그 값의 요소가 없습니다.");
+		}else {
+			System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+		}
+	}
+}

5c200650f105abb59c28ead49c1e92e65dc2aaad
junyoungKim <wnsdud1900427@gmail.com>
2025-11-22 16:18:31 +0900

    fix: 3sum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -31,11 +31,11 @@ class Solution {
                     while(left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
-                    right++;
+                    right--;
                 } else if (sum < 0) {
                     left++;
                 } else {
-                    right++;
+                    right--;
                 }
             }
         }

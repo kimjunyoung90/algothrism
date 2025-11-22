82415f227d15b590f2938e10aa269fed154847b4
junyoungKim <wnsdud1900427@gmail.com>
2025-11-22 15:55:25 +0900

    feat: 3sum

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -10,7 +10,6 @@ class Solution {
         // 1. 요소가 중복되지 않는 3개의 숫자 조합을 찾음
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
-                if (i == j) continue;
                 for (int k = 0; k < nums.length; k++) {
                     if (i == j || j == k || k == i) continue;
 

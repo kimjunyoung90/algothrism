b9748d5fd6e252da3712fda6a8b5caf444364894
junyoungKim <wnsdud1900427@gmail.com>
2026-01-03 19:38:39 +0900

    ValidParentheses

---
+++ src/ValidParentheses.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+/**
+ * @link https://leetcode.com/problems/valid-parentheses/description/
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+
+        //엣지 케이스
+        if(s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else {
+
+                if (stack.isEmpty()) return false;
+
+                char open = stack.pop();
+
+                switch(open) {
+                    case '[' -> { if(c != ']') return false; }
+                    case '{' -> { if(c != '}') return false; }
+                    case '(' -> { if(c != ')') return false; }
+                }
+
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}

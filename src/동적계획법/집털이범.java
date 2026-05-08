d6e44755efca1651642441e538fe336e998c309b
김준영 <rlawnsdud05@douzone.com>
2026-05-08 11:15:50 +0900

    .

--- src/동적계획법/집털이범.java
+++ src/동적계획법/집털이범.java
@@ -15,16 +15,15 @@ package 동적계획법;
 public class 집털이범 {
 	public int rob(int[] nums) {
 		//edge case
-		if(nums.length == 1) return nums[0];
+		if (nums.length == 1) return nums[0];
 		int prevPrevMax = nums[0];
 		int prevMax = Math.max(nums[0], nums[1]);
-		int max = Math.max(prevPrevMax, prevMax);
 		for (int i = 2; i < nums.length; i++) {
-			max = Math.max(nums[i] + prevPrevMax, prevMax);
+			int cur = Math.max(nums[i] + prevPrevMax, prevMax);
 			prevPrevMax = prevMax;
-			prevMax = max;
+			prevMax = cur;
 		}
-		return max;
+		return prevMax;
 	}
 
 	public static void main(String[] args) {

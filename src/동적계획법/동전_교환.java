d4a40335124eaee0f47d6c1d497a672a78da8af4
Junyoung Kim <wnsdud1900427@gmail.com>
2026-05-13 01:38:57 +0900

    .

--- src/동적계획법/동전_교환.java
+++ src/동적계획법/동전_교환.java
@@ -1,12 +1,14 @@
 package 동적계획법;
 
+import java.util.Arrays;
+
 /**
  * 동전_교환 (The Coin Change Problem)
- *
+ * <p>
  * 주어진 금액 n과 동전 종류 c[]가 있을 때,
  * 해당 금액을 만들 수 있는 서로 다른 방법의 수를 구하는 문제.
  * 각 동전은 무한히 사용할 수 있다.
- *
+ * <p>
  * 제약조건:
  * - 동전 종류는 모두 서로 다르다.
  * - 결과값은 32비트 정수 범위를 초과할 수 있다 (long 사용).
@@ -15,20 +17,42 @@ package 동적계획법;
  * @link https://www.hackerrank.com/challenges/coin-change/problem
  */
 public class 동전_교환 {
-    public long getWays(int n, int[] c) {
-        // TODO(human): 구현하세요.
-        return 0L;
-    }
+	int count = 1;
+	private int[][] memo;
 
-    public static void main(String[] args) {
-        동전_교환 test = new 동전_교환();
+	public long getWays(int n, int[] c) {
+		// TODO(human): 구현하세요.
+		memo = new int[n + 1][c.length];
+		for (int[] ints : memo) {
+			Arrays.fill(ints, -1);
+		}
+		return getTotalCount(0, c, n, 0);
+	}
 
-        // Sample 0: n=4, coins=[1, 2, 3] -> 4
-        long result0 = test.getWays(4, new int[]{1, 2, 3});
-        System.out.println(result0); // 기대값: 4
+	public int getTotalCount(int startIdx, int[] coins, int target, int accum) {
+		if (accum == target) return 1;
+		if (accum > target) return 0;
+		if (memo[accum][startIdx] != -1) {
+			return memo[accum][startIdx];
+		}
 
-        // Sample 1: n=10, coins=[2, 5, 3, 6] -> 5
-        long result1 = test.getWays(10, new int[]{2, 5, 3, 6});
-        System.out.println(result1); // 기대값: 5
-    }
+		int totalCount = 0;
+		for (int i = startIdx; i < coins.length; i++) {
+			totalCount += getTotalCount(i, coins, target, accum + coins[i]);
+		}
+		memo[accum][startIdx] = totalCount;
+		return totalCount;
+	}
+
+	public static void main(String[] args) {
+		동전_교환 test = new 동전_교환();
+
+		// Sample 0: n=4, coins=[1, 2, 3] -> 4
+		long result0 = test.getWays(4, new int[]{1, 2, 3});
+		System.out.println(result0); // 기대값: 4
+
+		// Sample 1: n=10, coins=[2, 5, 3, 6] -> 5
+		long result1 = test.getWays(10, new int[]{2, 5, 3, 6});
+		System.out.println(result1); // 기대값: 5
+	}
 }

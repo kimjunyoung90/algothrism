7c6ad1eb504cc5f7acc55be1b84c9ed67976fade
김준영 <rlawnsdud05@douzone.com>
2026-05-14 12:49:22 +0900

    중복 연산 제거

--- src/동적계획법/최소_동전_개수.java
+++ src/동적계획법/최소_동전_개수.java
@@ -26,19 +26,20 @@ public class 최소_동전_개수 {
         //떠오르는데로 해보자.
         //정렬 default는 오름차순
         min = Integer.MAX_VALUE;
-        getUsedCount(amount, 0, 0, coins);
+        getUsedCount(amount, 0, 0, coins, 0);
         return min == Integer.MAX_VALUE ? -1 : min;
     }
 
-    public void getUsedCount(int target, int accum, int depth, int[] coins) {
+    public void getUsedCount(int target, int accum, int depth, int[] coins, int start) {
 //        if(depth > min) return;
         if(target == accum) {
             min = Math.min(depth, min);
             return;
         }
         if(accum > target) return;
-		for (int coin : coins) {
-			getUsedCount(target, accum + coin, depth + 1, coins);
+		for (int i = start; i < coins.length; i++) {
+            int coin = coins[i];
+			getUsedCount(target, accum + coin, depth + 1, coins, i);
 		}
     }
 

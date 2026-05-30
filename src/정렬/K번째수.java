fc8d5d8414d5bb56fd51dc097f3d35e69fbc022a
Junyoung Kim <wnsdud1900427@gmail.com>
2026-05-30 23:18:01 +0900

    .

--- src/정렬/K번째수.java
+++ src/정렬/K번째수.java
@@ -1,24 +1,38 @@
 package 정렬;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
- * @link https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java
+ * K번째수
+ *
+ * 배열 array의 i번째 숫자부터 j번째 숫자까지 자른 뒤 정렬했을 때,
+ * k번째에 있는 수를 구한다. commands의 각 [i, j, k]에 대해 결과를 모아 반환한다.
+ *
+ * 제약조건:
+ * - array의 길이: 1 이상 100 이하
+ * - array의 각 원소: 1 이상 100 이하
+ * - commands의 길이: 1 이상 50 이하
+ * - commands의 각 원소의 길이: 3
+ *
+ * @link https://school.programmers.co.kr/learn/courses/30/lessons/42748
  */
 public class K번째수 {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for (int i = 0; i < commands.length ;i++) {
-            int[] command = commands[i];
+        // TODO(human): 구현하세요.
+        int[] result = new int[commands.length];
+
+        int i = 0;
+        for (int[] command : commands) {
             int from = command[0] - 1;
             int to = command[1];
-            int target = command[2] - 1;
-
-            int[] sliced = Arrays.copyOfRange(array, from, to);
-            Arrays.sort(sliced);
-            answer[i] = sliced[target];
+            int th = command[2] - 1;
+            int[] sub = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(sub);
+            result[i++] = sub[th];
         }
-        return answer;
+        //k번째에 있는 수 구하기
+        return result;
     }
 
     public static void main(String[] args) {
@@ -30,6 +44,6 @@ public class K번째수 {
                 {1, 7, 3}
         };
         int[] result = test.solution(array, commands);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result)); // 기대값: [5, 6, 3]
     }
 }

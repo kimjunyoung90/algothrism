53b89e3ab0d0b827a8b3325aea00311300733b4a
김준영 <rlawnsdud05@douzone.com>
2026-06-01 12:47:37 +0900

    .

--- src/정렬/HIndex.java
+++ src/정렬/HIndex.java
@@ -3,26 +3,43 @@ package 정렬;
 import java.util.Arrays;
 
 /**
+ * H-Index
+ * <p>
+ * 어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고
+ * 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 그 과학자의 H-Index이다.
+ * <p>
+ * 제약조건:
+ * - 과학자가 발표한 논문의 수: 1편 이상 1,000편 이하
+ * - 각 논문의 인용 횟수: 0회 이상 10,000회 이하
+ *
  * @link https://school.programmers.co.kr/learn/courses/30/lessons/42747
  */
 public class HIndex {
-    public int solution(int[] citations) {
-        Arrays.sort(citations);
-        int total = citations.length;
+	public int solution(int[] citations) {
+		int answer;
+		Arrays.sort(citations);
 
-        for (int i = 0; i < total; i++) {
-            int h번이상인용된논문의수 = total - i;
-            int 인용횟수 = citations[i];
-            if(인용횟수 >= h번이상인용된논문의수) return citations[i];
-        }
-        //논문 1편 발표한 경우
-        return citations[0];
-    }
+		int middle = citations[citations.length / 2];
+		while (true) {
+			int right = citations.length - middle;
+			int left = middle + 1; //0번 포함
+			if (left > right) {
+				middle--;
+			} else if (left < right) {
+				middle++;
+			}
+			if (right == left) {
+				answer =  citations[middle];
+				break;
+			}
+		}
+		return answer;
+	}
 
-    public static void main(String[] args) {
-        HIndex test = new HIndex();
-        int[] citations = {3, 0, 6, 1, 5};
-        int result = test.solution(citations);
-        System.out.println(result);
-    }
+	public static void main(String[] args) {
+		HIndex test = new HIndex();
+		int[] citations = {3, 0, 6, 1, 5};
+		int result = test.solution(citations);
+		System.out.println(result); // 기대값: 3
+	}
 }

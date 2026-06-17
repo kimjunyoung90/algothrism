eb56232fd50ed34e8b9ef098d23ea1d836a0cfd4
김준영 <rlawnsdud05@douzone.com>
2026-06-17 14:00:00 +0900

    .

--- src/탐욕/조이스틱.java
+++ src/탐욕/조이스틱.java
@@ -14,31 +14,35 @@ package 탐욕;
  * @link https://school.programmers.co.kr/learn/courses/30/lessons/42860
  */
 public class 조이스틱 {
-    public static final char upDefault = 'A';
-    public static final double downDefault = 'Z' + 1;
+    public static final char DEFAULT_CH = 'A';
+    public static final char LAST_CH = 'Z';
 
     public int solution(String name) {
-        // TODO(human): 구현하세요.
-        // 조이스틱을 조작해 알파벳 이름을 완성
-        // 처음에 모든 자리가 'A'로 시작
-        // 위 아래로 알파벳을 변경하고 좌 우로 커서 이동
-        // 최소 조작 횟수
-        // 첫번째 문자부터 끝에 문자까지 계속해서 이동하면서 체크하고
-        // 'A' 가 타겟 문자가 되기 위해서 조이스틱을 위로 조작하는게 빠를지, 아래로 조작하는게 빠를지 확인한 뒤 더 빠른 방식으로 조작 횟수를 갱신한다.
-        int answer = 0;
+        int n = name.length();
 
-        for (char c : name.toCharArray()) {
-            //문자는 숫자값이 인코딩 된 형태
-            //그래서 문자 끼리 연산을 하면 어느 정도 차이가 발생하는지 알 수 있음
-            // ex) A에서 Z라는 수를 만들기 위해 위로 조이스틱을 돌릴지(A -> B -> C -> D .... Z)
-            //90(Z) - 65(A) = 25 번 조이스틱을 움직여야함
-            if(c == upDefault) continue;
-            int min = (int) Math.min(c - upDefault, downDefault - c);
-            answer += min;
+        // 1) 상・하 비용: 자리마다 확정이므로 전부 더한다.
+        int upDown = 0;
+        for (int i = 0; i < n; i++) {
+            char current = name.charAt(i);
+            upDown += Math.min(current - DEFAULT_CH, LAST_CH - current + 1);
         }
-        //좌우 이동 횟수
-        answer += name.length() - 1;
-        return answer;
+
+        // 2) 좌・우 비용: 끝까지 직진(n-1)을 기본값으로 두고, 더 싼 꺾기 경로로 갱신한다.
+        int move = n - 1;
+        for (int i = 0; i < n; i++) {
+            // i 다음부터 연속된 A 구간을 건너뛰어 다음 비-A 위치(next)를 찾는다.
+            int next = i + 1;
+            while (next < n && name.charAt(next) == DEFAULT_CH) {
+                next++;
+            }
+
+            // TODO(human): 두 꺾기 경로의 비용을 계산해 move를 최소값으로 갱신하세요.
+            //  - 오른쪽 i까지 갔다가 되돌아 왼쪽 끝으로
+            //  - 왼쪽 끝으로 먼저 갔다가 오른쪽 i까지
+            //모르겠다..
+        }
+
+        return upDown + move;
     }
 
     public static void main(String[] args) {
